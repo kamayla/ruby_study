582edca630e7fed5abe9a9c96a416a4682c8c5a7
上村一平 <>
2018-04-06 18:56:46 +0900

    myblog init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address: 'smtp.senderid.net',
+    address: 'smtp.sendgrid.net',
     port: 587,
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
